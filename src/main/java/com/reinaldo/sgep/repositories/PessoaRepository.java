fe1d2be5cd3df0515ba6fd2d6595b8fbeb267e12
Reinaldo <lunajf18@gmail.com>
2019-03-29 00:26:09 -0300

    Camadas repository e service criados para Pessoa

---
+++ src/main/java/com/reinaldo/sgep/repositories/PessoaRepository.java
@@ -0,0 +1,9 @@
+package com.reinaldo.sgep.repositories;
+
+import com.reinaldo.sgep.domain.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
+}
---
+++ src/main/java/com/reinaldo/sgep/services/PessoaService.java
@@ -0,0 +1,20 @@
+package com.reinaldo.sgep.services;
+
+import com.reinaldo.sgep.domain.Pessoa;
+import com.reinaldo.sgep.repositories.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PessoaService {
+
+    @Autowired
+    private PessoaRepository repo;
+
+    public Pessoa buscar(Integer id){
+        Optional<Pessoa> obj = repo.findById(id);
+        return obj.orElse(null);
+    }
+}
