53f1e70aff369297e777390c6e329fbbe41a5906
Reinaldo <lunajf18@gmail.com>
2019-03-28 22:48:11 -0300

    Projeto criado

---
+++ src/main/java/com/reinaldo/sgep/SgepApplication.java
@@ -0,0 +1,13 @@
+package com.reinaldo.sgep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reinaldo/sgep/SgepApplicationTests.java
@@ -0,0 +1,16 @@
+package com.reinaldo.sgep;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SgepApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
